1fa6f73bbb437be4810ebbf1e502aba1335cae80
JustForStephan <justforstephan59@gmail.com>
2022-10-24 23:04:50 +0200

    colors in globals

---
+++ projectilm/lib/global.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+//different colors
+
+const fontColor = Color(0xFFffffff);
+const backgroundColor = Color(0xFF268223);
+const themeColor = Color(0xFF00BCF6);
+
+// 
--- projectilm/lib/mainWidget.dart
+++ projectilm/lib/mainWidget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'global.dart';
 
 class mainWidget extends StatefulWidget {
   const mainWidget({super.key, required this.title});
@@ -17,29 +18,40 @@ class _mainWidgetState extends State<mainWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (BuildContext context) {
-            return const IconButton(
-              icon: Icon(Icons.search),
-              onPressed: null,
-              // onPressed: () {
-              //   Scaffold.of(context).openDrawer();
-              // },
-              // tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-            );
-          },
-        ),
-        title: Form(
-          key: this._formKey,
-          child: TextFormField(
-            keyboardType:
-                TextInputType.text, // Use email input type for emails.
-            decoration: const InputDecoration(hintText: 'Suche'),
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return const IconButton(
+                icon: Icon(Icons.search),
+                onPressed: null,
+                // onPressed: () {
+                //   Scaffold.of(context).openDrawer();
+                // },
+                // tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
           ),
-        ), //title aof appbar
-        backgroundColor: const Color(0xFF268223), //background color of appbar
-      ),
+          title: Form(
+            key: this._formKey,
+            child: TextFormField(
+              keyboardType:
+                  TextInputType.text, // Use email input type for emails.
+              decoration: const InputDecoration(hintText: 'Suche'),
+            ),
+          ), //title aof appbar
+          backgroundColor: const Color(0xFF268223), //background color of appbar
+        ),
+        body: Column(
+          children: <Widget>[],
+        ));
+  }
+
+  // buttons and others
+
+  Widget groups(name, description) {
+    return Container(
+      child: Text(name, style: TextStyle(color: fontColor)),
+      padding:
     );
   }
 }
