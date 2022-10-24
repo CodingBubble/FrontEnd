b6cf8ebe3b61d10af7e185e46872aba40c1bc63d
JustForStephan <justforstephan59@gmail.com>
2022-10-25 01:21:08 +0200

    Die Scrollbar ist eingerichtet, man kann verschiedenen Gruppen innerhalb einer Liste in der Scrollbar anzeigen lassen

--- projectilm/lib/global.dart
+++ projectilm/lib/global.dart
@@ -5,4 +5,13 @@ const fontColor = Color(0xFFffffff);
 const backgroundColor = Color(0xFF268223);
 const themeColor = Color(0xFF00BCF6);
 
-// 
+// padding and margin
+
+const constPadding = EdgeInsets.all(30);
+const constMargin = EdgeInsets.only(top: 30, bottom: 30, left: 10, right: 10);
+const discanceBetweenWidgets = 8.0;
+
+// Size of different fonts
+
+const HeadfontOfWidget = 30.0;
+const SecondfontOfWidget = 20.0;
