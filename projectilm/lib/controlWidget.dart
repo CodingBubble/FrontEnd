cfdf212c73ec85089b940dc9ab08ef3b71c788ff
Luca Schirmer <acu1410l@gmail.com>
2022-10-25 23:50:03 +0200

    update accounts page
    unfinished apphandler => bug

--- projectilm/lib/controlWidget.dart
+++ projectilm/lib/controlWidget.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'mainWidget.dart';
 import 'global.dart';
 import 'login.dart';
+import 'src/projectillm_bridgelib_base.dart';
 
 class controlWidget extends StatelessWidget {
   const controlWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    if (1 == 1) {
-      //switched to the case mainWidget, only to test my design
+    //  Real User has to be called here not standard => make it dynamic later
+    var realUser = new User(1, 'Jakob', is_me: false);
+    if (realUser.is_me) {
       return MaterialApp(
         title: 'Grouping',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          backgroundColor: themeColor,
         ),
         home: const mainWidget(title: 'Grouping'),
       );
@@ -23,10 +26,63 @@ class controlWidget extends StatelessWidget {
         title: 'Grouping',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          backgroundColor: themeColor,
         ),
         home: const logInWidget(title: 'Grouping'),
       );
     }
   }
 }
+
+class appHandler extends StatelessWidget {
+  appHandler({Key? key}) : super(key: key);
+
+  static const String title = 'Grouping';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp.router(
+        routerDelegate: _router.routerDelegate,
+        routeInformationParser: _router.routeInformationParser,
+        routeInformationProvider: _router.routeInformationParser,
+      );
+
+  final GoRouter _router = GoRouter(
+    // errorBuilder: (context, state) => ErrorScreen(error: state.error),
+    routes: <GoRoute>[
+      GoRoute(
+        routes: <GoRoute>[
+          GoRoute(
+            path: 'passwordResetWidget',
+            pageBuilder: (BuildContext context, GoRouterState state) =>  MaterialPage<void>(child: ,  key: ValueKey<String>(state.location +
+                            DateTime.now().millisecondsSinceEpoch.toString(),),
+                const passwordResetWidget(),
+          ),),
+        ],
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+            const controlWidget(),
+      ),
+    ],
+  );
+}
+// routes: {
+//   '/': (context) => controlWidget(),
+//   '/passwordResetWidget': (context) => passwordResetWidget(),
+// },
+
+
+//   GoRoute(
+//     routes: <GoRoute>[
+//       GoRoute(
+//         path: 'passwordResetWidget',
+//         builder: (BuildContext context, GoRouterState state) {
+//           return const passwordResetWidget();
+//         },
+//       ),
+//     ],
+//     path: '/',
+//     builder: (BuildContext context, GoRouterState state) {
+//       return const controlWidget();
+//     },
+//   ),
+// ],
