11f0c76f0f516b5b48c35bfec7d674be925db88c
JustForStephan <justforstephan59@gmail.com>
2022-11-10 22:52:55 +0100

    die groupsWidget.dart ist jetzt bis auf Feinheiten gefixt und es gibt einen BACK button

--- projectilm/lib/groups-Widget.dart
+++ projectilm/lib/groups-Widget.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:projectilm/controlWidget.dart';
 import 'global.dart';
 
 class GroupsWidget extends StatefulWidget {
@@ -15,38 +18,47 @@ class _StateGroups extends State<GroupsWidget> {
       debugShowCheckedModeBanner: false,
       title: "Deine Gruppen",
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: backgroundColor,
-            title: Container(
-              child: Row(
-                children: [
-                  Builder(builder: (BuildContext context) {
-                    return (IconButton(
-                      icon: Icon(Icons.search),
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          title: Container(
+            child: Row(
+              children: [
+                Builder(builder: (BuildContext context) {
+                  return (IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    color: WidgetColor,
+                    onPressed: () => {
+                      AppHandler("mainWidget", context),
+                    },
+                  )); // here to add the onPressed-command to search something
+                }),
+                Builder(builder: (BuildContext context) {
+                  return (IconButton(
+                    icon: Icon(Icons.search),
+                    color: WidgetColor,
+                    onPressed: () => {},
+                  )); // here to add the onPressed-command to search something
+                }),
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text("Your Groups",
+                        style: TextStyle(color: textColor))),
+                Builder(builder: (BuildContext context) {
+                  return (IconButton(
+                      icon: Icon(Icons.settings),
                       color: WidgetColor,
-                      onPressed: () => {},
-                    )); // here to add the onPressed-command to search something
-                  }),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.62,
-                      child: Text("Your Groups",
-                          style: TextStyle(color: textColor))),
-                  Builder(builder: (BuildContext context) {
-                    return (IconButton(
-                        icon: Icon(Icons.settings),
-                        color: WidgetColor,
-                        onPressed: () =>
-                            {})); //here to add the onPressed-command to navigate to settings
-                  })
-                ],
-              ),
+                      onPressed: () =>
+                          {})); //here to add the onPressed-command to navigate to settings
+                })
+              ],
             ),
           ),
-          body: Column(
-            children: [
-              //first widget as official chat
-              Container(
-                  child: Column(
+        ),
+        body: Column(
+          children: [
+            //first widget as official chat
+            Container(
+              child: Column(
                 children: [
                   new Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
                   Container(
@@ -61,43 +73,14 @@ class _StateGroups extends State<GroupsWidget> {
                     ),
                   )
                 ],
-              )),
-              //second widget as Scrollbar for all events
-              new Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
-              Scrollbar(
-                  child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return Material(
-                    child: Column(
-                      children: [
-                        new Padding(
-                            padding: EdgeInsets.all(discanceBetweenWidgets)),
-                        Container(
-                          width: MediaQuery.of(context).size.width *
-                              0.9, // the distance to the margin of display
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                  WidgetColor,
-                                ),
-                              ),
-                              child: eventWidget(
-                                eventNames[index],
-                                eventDescriptions[index],
-                              ),
-                              onPressed: () => {
-                                    /*Navigator.push(context, new MaterialPageRoute(builder: (context) => ))*/
-                                  }),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                itemCount: eventNames.length,
-              ))
-            ],
-          )),
+              ),
+            ),
+            //second widget as Scrollbar for all events
+            new Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
+            scrollEvents()
+          ],
+        ),
+      ),
     );
   }
 
@@ -153,5 +136,42 @@ class _StateGroups extends State<GroupsWidget> {
       margin: constMargin,
       width: double.infinity,
     );
+  }
+
+  Widget scrollEvents() {
+    return SizedBox(
+        height: MediaQuery.of(context).size.height * 0.6,
+        child: Scrollbar(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Material(
+                child: Column(
+                  children: [
+                    new Padding(
+                        padding: EdgeInsets.all(discanceBetweenWidgets)),
+                    Container(
+                      width: MediaQuery.of(context).size.width *
+                          0.9, // the distance to the margin of display
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            WidgetColor,
+                          ),
+                        ),
+                        child: eventWidget(
+                          eventNames[index],
+                          eventDescriptions[index],
+                        ),
+                        onPressed: () => {},
+                        // first parameter is the keyword to the next widget, other is the context-builder for the nativigator-class, just copy and past it
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            itemCount: eventNames.length,
+          ),
+        ));
   }
 }
