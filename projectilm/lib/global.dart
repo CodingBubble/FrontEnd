3722c9d837ec795afb44d3af192e1095222552a1
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2022-12-22 02:07:58 +0100

    update User Settings
    : ) :+1:

--- projectilm/lib/global.dart
+++ projectilm/lib/global.dart
@@ -6,14 +6,15 @@ const backgroundColor = Colors.white;
 const primaryTextColor = Color.fromARGB(255, 76, 76, 76);
 const secondaryTextColor = Color.fromARGB(255, 0, 0, 0);
 const WidgetColor = Color.fromARGB(255, 150, 150, 150);
-// padding and margin
+const variationColor = Color.fromARGB(255, 45, 92, 147);
 
+// padding and margin
 const constPadding = EdgeInsets.all(10);
 const constMargin = EdgeInsets.only(top: 30, bottom: 30, left: 10, right: 10);
 const discanceBetweenWidgets = 8.0;
 
 // Size of different fonts
-
+const GigafontOfWidget = 40.0;
 const HeadfontOfWidget = 30.0;
 const SecondfontOfWidget = 20.0;
 
--- projectilm/lib/settingsWidget.dart
+++ projectilm/lib/settingsWidget.dart
@@ -26,12 +26,13 @@ class _SettingsWidgetState extends State<SettingsWidget> {
           title: Row(
             children: [
               IconButton(
-                icon: Icon(Icons.arrow_back),
+                icon: const Icon(Icons.arrow_back),
                 onPressed: () {
-                  AppHandler("mainWidget", context, []);
+                  AppHandler("mainWidget", context, const []);
                 },
               ),
-              Text("Einstellungen", style: TextStyle(color: backgroundColor))
+              const Text("Einstellungen",
+                  style: TextStyle(color: backgroundColor))
             ],
           ),
           backgroundColor: WidgetColor,
@@ -58,71 +59,193 @@ class _SettingsWidgetState extends State<SettingsWidget> {
 }
 
 List<Widget> settingCathegories = <Widget>[
-  generalSettings(),
   themeSettings(),
-  securitySettings()
+  generalSettings(),
+  // securitySettings()
 ];
 
 Widget generalSettings() {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   return (Container(
-      child: Column(
-        children: [
-          //headline
-          Text("Allgemein"),
-          ElevatedButton(
-            child: Container(
-              child: Text("...",
-                  style: TextStyle(
-                      color: secondaryTextColor, fontSize: SecondfontOfWidget)),
-            ),
-            onPressed: () {},
-          )
-        ],
-      ),
       padding: constPadding,
       margin: constMargin,
       width: double.infinity,
-      color: WidgetColor));
+      color: WidgetColor,
+      child: Column(
+        children: [
+          //headline
+          const Text(
+            "Account",
+            style:
+                TextStyle(color: primaryTextColor, fontSize: GigafontOfWidget),
+          ),
+          const Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Gebe dein neues Passwort ein',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Bitte gebe ein neues Passwort ein';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                      }
+                    },
+                    child: const Text('Passwort ändern'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
+        ],
+      )));
 }
 
 Widget themeSettings() {
   return (Container(
-      child: Column(
-        children: [
-          //headline
-          Text("Design"),
-          ElevatedButton(
-              child: Text("...",
-                  style: TextStyle(
-                      color: secondaryTextColor, fontSize: SecondfontOfWidget)),
-              onPressed: () {}),
-          Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
-          ElevatedButton(child: Text("..."), onPressed: () {}),
-        ],
-      ),
       padding: constPadding,
       margin: constMargin,
       width: double.infinity,
-      color: WidgetColor));
+      color: WidgetColor,
+      child: Column(
+        children: [
+          //headline
+          const Text(
+            "Aussehen",
+            style:
+                TextStyle(color: primaryTextColor, fontSize: GigafontOfWidget),
+          ),
+          const Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
+          SizedBox(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 50,
+                    icon: const Icon(Icons.sunny, color: Colors.yellow)),
+                const Padding(
+                  padding: EdgeInsets.all(discanceBetweenWidgets),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 50,
+                    icon: const Icon(Icons.shield_moon)),
+                const Padding(
+                  padding: EdgeInsets.all(discanceBetweenWidgets),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.color_lens_outlined),
+                    iconSize: 50,
+                    color: Colors.brown),
+                const Padding(
+                  padding: EdgeInsets.all(discanceBetweenWidgets),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(discanceBetweenWidgets),
+                  ),
+                  ColoredBox(
+                    color: variationColor,
+                    child: Container(
+                      width: double.infinity,
+                      child: Column(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.all(discanceBetweenWidgets),
+                          ),
+                          Text(
+                            'Überschrift',
+                            style: TextStyle(
+                              color: primaryTextColor,
+                              fontSize: SecondfontOfWidget,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(discanceBetweenWidgets),
+                          ),
+                          Text(
+                            'Text',
+                            style: TextStyle(
+                              color: secondaryTextColor,
+                              fontSize: SecondfontOfWidget,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(discanceBetweenWidgets),
+                          ),
+                          Text(
+                            'Boxen',
+                            style: TextStyle(
+                              color: WidgetColor,
+                              fontSize: SecondfontOfWidget,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(discanceBetweenWidgets),
+                          ),
+                          Text(
+                            'Hintergrund',
+                            style: TextStyle(
+                              color: backgroundColor,
+                              fontSize: SecondfontOfWidget,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(discanceBetweenWidgets),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ],
+      )));
 }
 
-Widget securitySettings() {
-  return (Container(
-      child: Column(
-        children: [
-          //headline
-          Text("Datenschutz und Sicherheit"),
-          ElevatedButton(
-              child: Text("...",
-                  style: TextStyle(
-                      color: primaryTextColor, fontSize: SecondfontOfWidget)),
-              onPressed: () {}),
-          Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
-          ElevatedButton(child: Text("..."), onPressed: () {})
-        ],
-      ),
-      padding: constPadding,
-      margin: constMargin,
-      width: double.infinity,
-      color: WidgetColor));
-}
+// Widget securitySettings() {
+//   return (Container(
+//       padding: constPadding,
+//       margin: constMargin,
+//       width: double.infinity,
+//       color: WidgetColor,
+//       child: Column(
+//         children: [
+//           //headline
+//           const Text("Datenschutz und Sicherheit"),
+//           ElevatedButton(
+//               child: const Text("...",
+//                   style: TextStyle(
+//                       color: textColor, fontSize: SecondfontOfWidget)),
+//               onPressed: () {}),
+//           const Padding(padding: EdgeInsets.all(discanceBetweenWidgets)),
+//           ElevatedButton(child: const Text("..."), onPressed: () {})
+//         ],
+//       )));
+// }
