92dc027c42bda850f85dd57bc08144e7a7ab6671
Luca Schirmer <acu1410l@gmail.com>
2023-03-14 18:04:03 +0100

    update no Groups Screen
    only for groups implemented yet => to do:
    
    - events
    - chat?
    - Umfragen
    - Einkaufslisten

--- projectilm/lib/group/mainWidget.dart
+++ projectilm/lib/group/mainWidget.dart
@@ -33,130 +33,236 @@ class _mainWidgetState extends State<mainWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: backgroundColor,
-        appBar: get_user_app_bar(context, searchFilter),
-        body: Scrollbar(
-            child: ListView.builder(
-          itemBuilder: (context, index) {
-            return Material(
-              color: backgroundColor,
+    // Screen if no groups yet
+    if (groups_glob.isEmpty) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: backgroundColor,
+          appBar: get_user_app_bar(context, searchFilter),
+          body: Container(
+            padding: constPadding,
+            child: Center(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(discanceBetweenWidgets),
-                    child: Container(
-                      color: backgroundColor,
-                      width: MediaQuery.of(context).size.width *
-                          0.9, // the distance to the margin of display
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                            widgetColor,
-                          ),
+                    padding: const EdgeInsets.only(top: 2),
+                    child: Center(
+                      child: Text(
+                        "WILLKOMMEN IN",
+                        style: TextStyle(
+                          fontSize: SecondfontOfWidget,
+                          color: secondaryTextColor,
+                          fontWeight: FontWeight.w600,
                         ),
-                        child: groups(groups_glob[index], pinned[index]),
-                        onPressed: () {
-                          current_group = groups_glob[index];
-                          AppHandler("groupWidget", context, []);
-                        },
-                        // first parameter is the keyword to the next widget, other is the context-builder for the nativigator-class, just copy and past it
                       ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: RichText(
+                      text: TextSpan(
+                        text: "G",
+                        style: const TextStyle(
+                          color: Colors.green,
+                          fontSize: GigafontOfWidget * 1.25,
+                          fontWeight: FontWeight.w400,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "rouping",
+                            style: TextStyle(
+                              color: primaryTextColor,
+                              fontSize: HeadfontOfWidget * 1.4,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 25),
+                  ),
+                  Text(
+                    "Noch keine Gruppe?",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: primaryTextColor,
+                      fontSize: SecondfontOfWidget,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 30),
+                  ),
+                  Text(
+                    "Klicke auf das Plus unten rechts um eine Gruppe hinzuzufügen.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: primaryTextColor,
+                      fontSize: SecondfontOfWidget,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ],
               ),
-            );
-          },
-          itemCount: groups_glob.length,
-        )),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            AppHandler("create_group", context, []);
-          },
-          mini: true,
-          backgroundColor: positiveColor,
-          child: const Icon(Icons.add_circle),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              AppHandler("create_group", context, const []);
+            },
+            mini: true,
+            backgroundColor: positiveColor,
+            child: const Icon(Icons.add_circle),
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: backgroundColor,
+          appBar: get_user_app_bar(context, searchFilter),
+          body: Scrollbar(
+              child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Material(
+                color: backgroundColor,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(discanceBetweenWidgets),
+                      child: Container(
+                        color: backgroundColor,
+                        width: MediaQuery.of(context).size.width *
+                            0.9, // the distance to the margin of display
+                        child: groups(groups_glob[index], pinned[index], index),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            itemCount: groups_glob.length,
+          )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              AppHandler("create_group", context, const []);
+            },
+            mini: true,
+            backgroundColor: positiveColor,
+            child: const Icon(Icons.add_circle),
+          ),
+        ),
+      );
+    }
   }
 
   List<Event> Groups = <Event>[];
 
   // buttons and others
 
-  Widget groups(Group g, bool b) {
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(vertical: 30),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // Wofür ist das Icon??
+  // Widget checkGroups(Group g, bool b, index) {
+  //   if (groups_glob.isEmpty) {
+  //     {
+  //       return Container(
+  //         padding: constPadding,
+  //         child: Column(
+  //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //           crossAxisAlignment: CrossAxisAlignment.center,
+  //           mainAxisSize: MainAxisSize.max,
+  //           children: const [
+  //             Text("Noch keine Gruppe?"),
+  //             Text(
+  //                 "Klicke auf das Plus unten rechts um eine Gruppe hinzuzufügen."),
+  //           ],
+  //         ),
+  //       );
+  //     }
+  //   } else {
+  //     return groups(g, b, index);
+  //   }
+  // }
 
-          // Icon(
-          //   Icons.group,
-          //   color: backgroundColor,
-          //   size: MediaQuery.of(context).size.width * 0.1,
-          // ),
-          Container(
-            alignment: Alignment.center,
-            width: MediaQuery.of(context).size.width * 0.7,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Text(
-                    g.name,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: primaryTextColor,
-                      fontSize: HeadfontOfWidget,
-                      fontWeight: FontWeight.w600,
+  Widget groups(Group g, bool b, index) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(
+          widgetColor,
+        ),
+      ),
+      child: Container(
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(vertical: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      g.name,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: primaryTextColor,
+                        fontSize: HeadfontOfWidget,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        g.description,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: secondaryTextColor,
-                          fontSize: descriptionfontOfWidget,
-                          fontWeight: FontWeight.w400,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          g.description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: secondaryTextColor,
+                            fontSize: descriptionfontOfWidget,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: IconButton(
-                        onPressed: () async {
-                          await toggleGroupPinned(g);
-                          loadGroups();
-                        },
-                        icon: Icon(
-                          b ? Icons.push_pin : Icons.push_pin_outlined,
-                          color: b ? primaryTextColor : backgroundColor,
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: IconButton(
+                          onPressed: () async {
+                            await toggleGroupPinned(g);
+                            loadGroups();
+                          },
+                          icon: Icon(
+                            b ? Icons.push_pin : Icons.push_pin_outlined,
+                            color: b ? primaryTextColor : backgroundColor,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+      onPressed: () {
+        current_group = groups_glob[index];
+        AppHandler("groupWidget", context, const []);
+      },
+      // first parameter is the keyword to the next widget, other is the context-builder for the nativigator-class, just copy and past it
     );
   }
 
